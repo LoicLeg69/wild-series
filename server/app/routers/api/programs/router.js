5db622f9390b1447735410552f9e98c00a9d0094
Loic legoff <loic95280@gmail.com>
2024-05-22 10:51:06 +0200

    quest 03 harmonia

--- server/app/routers/api/programs/router.js
+++ server/app/routers/api/programs/router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Import item-related actions
-const { browse } = require("../../../controllers/itemActions");
+const { browse } = require("../../../controllers/programActions");
 
 // Route to get a list of items
 router.get("/", browse);
--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Import And Use Routers Here
+const { sayWelcome } = require("../../controllers/sayActions");
+
+router.get("/", sayWelcome);
+
+// const { browse } = require("../../controllers/programActions");
+
+// router.get("/progam", browse);
+
 /* ************************************************************************* */
 
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
+
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 
 /* ************************************************************************* */
 
